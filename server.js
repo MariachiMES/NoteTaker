5cb841f437eadee8d9d49d9d95b9a8cee7f4cf5d
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-10-13 22:08:40 -0500

    added routes

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 const express = require("express");
-const path = require("path");
-const db = require("./db/db.json");
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
+const apiroutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlroutes");
 
-app.get("/", (req, res) => res.send("public/assets/index.html"));
+app.use(apiroutes);
 
-app.get("/assets", (req, res) =>
-  res.sendFile(path.join(__dirname, "assets/notes.html"))
-);
+app.use(htmlRoutes);
 
-app.get("/assets", (req, res) =>
-  res.sendFile(path.join(__dirname, "assets/notes.html"))
-);
-
-app.get("/api?terms/:term", (req, res) => {
-  const requestedTerm = req.params.term.toLowerCase();
+app.listen(PORT, function () {
+  console.log("server listening on PORT ", PORT);
 });
-
-for (let i = 0; i < requestedTerm.length; i++) {
-  if (requestedTerm === requestedTerm[i].term.toLowerCase()) {
-    return res.json(requestedTerm[i]);
-  }
-}
-app.listen(PORT, () =>
-  console.log(`Local Server Running at http://localhost:${PORT}`)
-);
